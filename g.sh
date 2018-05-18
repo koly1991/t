803974a0492b2b788229b6b107b6a4f1765af97d
x <victor.hdez.barrios@gmail.com>
2018-05-18 22:42:13 +0200

    autocommit of Fri May 18 22:42:13 CEST 2018

--- g.sh
+++ g.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/zsh
 if [ ! -z $1 ] && [ ! -z $2 ]; then
-	while [ ! -e /tmp/.autocommit ]; do
+	while [ ! -e /tmp/.stopautocommit ]; do
 		if ! git status | grep "nothing to commit, working tree clean"; then
 			git add .
 			git commit -m "autocommit of $(date)"
 			git push https://$1:$2@github.com/$1/$3
 		fi
+		sleep 0.5
 	done
 else
 	echo first argument is username, second one is password, third is repo name
-	echo recall that /tmp/.autocommit must exist
+	echo recall that /tmp/.stopautocommit stops everything
 fi
