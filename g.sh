d2471e13cc86c6e17a2db95b96d68b5b3f26b5fa
x <victor.hdez.barrios@gmail.com>
2018-05-18 22:40:26 +0200

    autocommit of Fri May 18 22:40:26 CEST 2018

--- g.sh
+++ g.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 if [ ! -z $1 ] && [ ! -z $2 ]; then
-	if git status | grep "nothing to commit, working tree clean"; then
+	if ! git status | grep "nothing to commit, working tree clean"; then
 		git add .
 		git commit -m "autocommit of $(date)"
 		git push https://$1:$2@github.com/$1/$3
