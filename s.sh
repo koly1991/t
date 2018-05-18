64ed497e96556e9a2861c964deee5075aa57328f
x <victor.hdez.barrios@gmail.com>
2018-05-19 00:57:59 +0200

    autocommit of Sa 19. Mai 00:57:59 CEST 2018

--- s.sh
+++ s.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/zsh
+#!/bin/bash
 if [ ! -z $1 ]; then
-	HOME=$(mktemp -d)
+	dir=$(mktemp -d)
+	HOME=$dir
 	git config user.email $1
 	git config user.name x
 	if ! chromium $(cat pdfs.list) /tmp/main.pdf &> /dev/null ; then
 		chromium-browser $(cat pdfs.list) /tmp/main.pdf &> /dev/null
 	fi &
 	screen 
+	rm -rf $dir
 fi
