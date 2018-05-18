9b79ebb9af8e791c3fc4cb63d3c0e872af4a2c31
x <victor.hdez.barrios@gmail.com>
2018-05-18 22:37:48 +0200

    finished apparently g.sh

--- g.sh
+++ g.sh
@@ -3,8 +3,8 @@ if [ ! -z $1 ] && [ ! -z $2 ]; then
 	if git status | grep "nothing to commit, working tree clean"; then
 		git add .
 		git commit -m "autocommit of $(date)"
-		git push $1:$2@github.com
+		git push https://$1:$2@github.com/$1/$3
 	fi
 else
-	echo first argument is username, second one is password
+	echo first argument is username, second one is password, third is repo name
 fi
