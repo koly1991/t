66e893c4ad4650db1926ccc43218723e915684df
x <victor.hdez.barrios@gmail.com>
2018-05-18 22:23:05 +0200

    changed start script

--- s.sh
+++ s.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/zsh
-HOME=$(mktemp -d)
-if ! chromium $(cat pdfs.list) /tmp/main.pdf &> /dev/null ; then
-	chromium-browser $(cat pdfs.list) /tmp/main.pdf &> /dev/null
-fi &
-screen 
+if [ ! -z $1 ]; then
+	HOME=$(mktemp -d)
+	git config user.email $1
+	git config user.name x
+	if ! chromium $(cat pdfs.list) /tmp/main.pdf &> /dev/null ; then
+		chromium-browser $(cat pdfs.list) /tmp/main.pdf &> /dev/null
+	fi &
+	screen 
+fi
